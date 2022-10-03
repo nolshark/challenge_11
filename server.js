9094d4f15ad1e4800307f024ed8c4d29a246db51
nolshark <108495035+nolshark@users.noreply.github.com>
2022-10-03 12:12:24 -0400

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const fs = require('fs');
 const path = require('path');
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3001;
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
